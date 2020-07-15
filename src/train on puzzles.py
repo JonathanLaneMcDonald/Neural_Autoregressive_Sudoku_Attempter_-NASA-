cc0cd0a6cfa7a6e9291c0d67e5a7fd496e8a60d1
lane <jonathan.lane.mcdonald@gmail.com>
2020-07-15 16:09:04 +1000

    updates to model, metrics, and notes

--- src/train on puzzles.py
+++ src/train on puzzles.py
@@ -35,54 +35,60 @@ from keras.models import Model
 from keras.layers import Input
 from keras.layers import Conv2D, Conv3D, Add
 from keras.layers import BatchNormalization, Activation, Dropout, Reshape
+from keras.initializers import RandomNormal
+from keras.optimizers import Adam
 
-def build_sudoku_model(filters, kernels, blocks, dilations=[(1,1),(1,1)]):
+def build_sudoku_model(filters, kernels, layers):
 	
 	input = Input(shape=(9,9,9))
 
-	x = Conv2D(filters=filters, kernel_size=kernels, padding='same')(input)
+	x = Conv2D(filters=filters, kernel_size=kernels, padding='same', kernel_initializer=RandomNormal(mean=0, stddev=0.01))(input)
 	x = BatchNormalization()(x)
 	x = Activation('relu')(x)
-	x = Dropout(0.2)(x)
 	
-	for _ in range(blocks):
+	conv = 2
+	blocks = layers//conv
+	for _1 in range(blocks):
 		y = x
-		for dilation in dilations:
-			y = Conv2D(filters=filters, kernel_size=kernels, padding='same', dilation_rate=dilation)(y)
+
+		for _2 in range(conv):
+			y = Conv2D(filters=filters, kernel_size=kernels, padding='same', kernel_initializer=RandomNormal(mean=0, stddev=0.01))(y)
 			y = BatchNormalization()(y)
 			y = Activation('relu')(y)
-			y = Dropout(0.2)(y)
+
 		x = Add()([x,y])
 
-	output = Conv2D(filters=9, kernel_size=(1,1), padding='same', activation='softmax')(x)
+	output = Conv2D(filters=9, kernel_size=(1,1), padding='same', activation='softmax', kernel_initializer=RandomNormal(mean=0, stddev=0.01))(x)
 
 	model = Model(inputs=input, outputs=output)
 	model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 	model.summary()
 	return model
 
-def build_3D_sudoku_model(filters, kernels, blocks, dilations=[(1,1,1),(1,1,1)]):
+def build_3D_sudoku_model(filters, kernels, layers):
 	
 	input = Input(shape=(9,9,9))
 
 	x = Reshape((9,9,9,1))(input)
-	x = Conv3D(filters=filters, kernel_size=kernels, padding='same')(x)
+	x = Conv3D(filters=filters, kernel_size=kernels, padding='same', kernel_initializer=RandomNormal(mean=0, stddev=0.01))(x)
 	x = BatchNormalization()(x)
 	x = Activation('relu')(x)
-	x = Dropout(0.2)(x)
 	
-	for _ in range(blocks):
+	conv = 2
+	blocks = layers//conv
+	for _1 in range(blocks):
 		y = x
-		for dilation in dilations:
-			y = Conv3D(filters=filters, kernel_size=kernels, padding='same', dilation_rate=dilation)(y)
+
+		for _2 in range(conv):
+			y = Conv3D(filters=filters, kernel_size=kernels, padding='same', kernel_initializer=RandomNormal(mean=0, stddev=0.01))(y)
 			y = BatchNormalization()(y)
 			y = Activation('relu')(y)
-			y = Dropout(0.2)(y)
+
 		x = Add()([x,y])
 
-	x = Conv3D(filters=1, kernel_size=(1,1,1), padding='same', activation='relu')(x)
+	x = Conv3D(filters=1, kernel_size=(1,1,1), padding='same', activation='relu', kernel_initializer=RandomNormal(mean=0, stddev=0.01))(x)
 	x = Reshape((9,9,9))(x)
-	output = Conv2D(filters=9, kernel_size=(1,1), padding='same', activation='softmax')(x)
+	output = Conv2D(filters=9, kernel_size=(1,1), padding='same', activation='softmax', kernel_initializer=RandomNormal(mean=0, stddev=0.01))(x)
 
 	model = Model(inputs=input, outputs=output)
 	model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
@@ -90,8 +96,46 @@ def build_3D_sudoku_model(filters, kernels, blocks, dilations=[(1,1,1),(1,1,1)])
 	return model
 
 import numpy as np
-from numpy.random import permutation, normal
+from numpy.random import permutation
 from numpy.random import random as npr
+
+def get_mae_for_solution_prediction_pair(puzzle, solution, prediction, per_unknown = True):
+	if per_unknown:
+		unknowns = 0
+		for r in range(9):
+			for c in range(9):
+				if np.sum(puzzle[r][c]) == 0:
+					unknowns += 1
+		if unknowns:
+			return np.sum(np.abs(solution - prediction)) / float(unknowns)
+		else:
+			return 0
+	else:
+		return np.sum(np.abs(solution - prediction))
+
+def get_hardest_n_puzzles_by_mae(puzzles, solutions, predictions, n):
+	if n:
+		scores = list(reversed(sorted([(get_mae_for_solution_prediction_pair(puzzles[x], solutions[x], predictions[x]), x) for x in range(len(puzzles))])))
+		return permutation([(puzzles[x[1]], solutions[x[1]]) for x in scores[:n]])
+	else:
+		return []
+
+def create_random_homomorphism_in_place(puzzle, solution):
+	sbox = {k:v for k,v in zip(range(9),permutation(range(9)))}
+
+	new_puzzle = np.zeros((9,9,9))
+	new_solution = np.zeros((9,9,9))
+	for r in range(9):
+		for c in range(9):
+			if np.sum(puzzle[r][c]):
+				puz_max = np.argmax(puzzle[r][c])
+				new_puzzle[r][c][sbox[puz_max]] = 1
+			
+			if np.sum(solution[r][c]):
+				sol_max = np.argmax(solution[r][c])
+				new_solution[r][c][sbox[sol_max]] = 1
+			
+	return new_puzzle, new_solution
 
 def create_random_homomorphism(line):
 	sbox = {'0':'0','\t':'\t'}
@@ -143,21 +187,28 @@ def to_sparse(data):
 			frame[r][c][data[i]-1] = 1
 	return frame
 
-def create_dataset(source, samples, for_validation=False):
+def create_dataset(source, samples, for_validation=False, puzzles_for_review = []):
 	puzzles = np.zeros((samples, 9, 9, 9),dtype=np.int8)
 	solutions = np.zeros((samples, 9, 9, 9),dtype=np.int8)
 	pencilmarks = np.zeros((samples, 9, 9, 9),dtype=np.int8)
+	
+	s = 0
+	for puzzle, solution in puzzles_for_review:
+		puzzles[s], solutions[s] = create_random_homomorphism_in_place(puzzle, solution)
+		s += 1
 
-	for s in range(samples):
+	print ('resampled data records:',s)
+	while s < samples:
 		puzzle, solution = create_puzzle_solution_pair(source[int(npr()*len(source))],int(for_validation)+max(0,npr()))
 		puzzles[s] = to_sparse(puzzle)
 		solutions[s] = to_sparse(solution)
-		pencilmarks[s] = generate_pencil_marks(puzzles[s])
+		#pencilmarks[s] = generate_pencil_marks(puzzles[s])
+		s += 1
 
 	return puzzles, solutions, pencilmarks
 
 def validate_predictions(puzzles, solutions, pencilmarks, model):
-	predicted = model.predict(pencilmarks)
+	predicted = model.predict(puzzles)
 
 	predictable = 0
 	accurate_predictions = 0
@@ -195,7 +246,7 @@ def autoregressive_validation(puzzles, solutions, pencilmarks, model):
 		working_puzzle = np.copy(puzzles[p])
 		
 		while number_of_unpredicted(working_puzzle):
-			(r,c), value = get_max_prediction(working_puzzle, model.predict(np.array([generate_pencil_marks(working_puzzle)]))[0])
+			(r,c), value = get_max_prediction(working_puzzle, model.predict(np.array([working_puzzle]))[0])
 			working_puzzle[r][c][value] = 1
 		
 		for r in range(9):
@@ -207,40 +258,146 @@ def autoregressive_validation(puzzles, solutions, pencilmarks, model):
 					
 	return accurate_predictions / predictable
 
-model = build_3D_sudoku_model(64, (3,3,3), 5)
-#model = build_sudoku_model(64, (3,3), 5)
-#model = build_sudoku_model(128, (3,3), 5)
-#model = build_sudoku_model(256, (3,3), 20)
+from keras.models import load_model
+
+'''
+the sudoku "paper"
+
+	why is sudoku an interesting problem from the point of view of machine learning?
+
+		used to fuzzy rules, but sudoku works with discrete values and logical relationships + it's my first crack at 3d convolutions
+
+		results
+			show a graph of the 5(?) models in training
+				show the loss graph
+				show the one-shot solution graph
+				show the autoregressive solution graph
+			
+			show a gif of the best model making pencilmarks and me just doing what it says, so you can watch the colors change in intensity :D
+	
+	concepts
+
+		sudoku
+			is this type of game with simple rules. feel free to gloss over and then offer a link to wikipedia/sudoku
+
+		data representation
+
+			sudoku is a 3d game, masquerading as a 2d game. its most natural representation is as a binary puzzle in 3d and to carry those through with conv3d
+		
+		data augmentation
+			
+			sudoku has properties that can be exploited for the purpose of data augmentation
+				
+				board symmetries, which i exploited in my reimplementation of the alphago paper, but don't care about here
+				
+				sudoku is strongly homomorphic - explain what that means and maybe give some examples?
+
+			random subsets
+
+				puzzles and solutions have been generated - this leaves us typically with a 50-60 step process to complete a puzzle
+
+				to augment data, we can select random subsets to set as knowns and predict the rest of the puzzle from that
+
+		autoregressive inference
+
+			autoregressive inference is used regularly in natural language processing and it is used here to great effect!
+
+			in this application, it just means trying to predict everything, like the model was trained to do, but then only making the single move with the highest confidence. this process is repeated until the puzzle is complete.
+			
+			the model is trained to predict solutions in one shot from a partially completed puzzle
+
+	methods
+
+		generation of puzzles and solutions
+
+		lots of opportunity for data augmentation (symmetries and homomorphisms)
+
+		deciding how to represent data as input/output, but also as it moves through the network
+
+			it only really makes sense to represent as binary at i/o, but:
+
+				we can use conv2d within the network, or
+
+				we can use conv3d within the network
+
+		during training, can we speed up training by replaying homomorphisms of difficult puzzles
+
+		post-training analysis
+
+			turns out i wrote this really old sudoku solver, which is also available
+
+			then i used that to analyze the results and see what it got right on a per-technique basis?
+
+do a model comparison where the models pretty much all have around 1.5 million weights
+
+	baseline model conv2d
+	make it better with conv3d
+	make it better by making it deeper
+	make it better with an improved training regime (replay difficult puzzles)
+	make it better by making it even deeper(?)
+	: the moral of the story is look how much better performance can be with good architecture, a good representation, and a good training regime
+	: more lessons in the form of objectively measuring performance instead of solely relying on loss and accuracy as reported by tf
+
+	baseline is one of the intermediate models used by leela go zero
+		3,3 x 128 x 10
+
+	jump straight to keeping a high fidelity internal repr with conv3d
+		3,3,3 x 32 x 50 (or something, to keep the weights roughly equal)
+
+	try making it deeper
+		3,3,3 x 16 x 200
+	
+	try training with replay
+		should accelerate learning
+	
+	fine-tune the model after training
+
+	make each of these models available in the repo
+	allow the user to load a specified model to play with in the play sudoku.py script
+'''
+
+#model = build_3D_sudoku_model(32, (3,3,3), 50)
+#model = build_sudoku_model(64, (3,3), 50)
+
+model = load_model('model - one-shot_acc=0.7975')
+model.compile(loss='categorical_crossentropy', optimizer=Adam(lr=0.0002), metrics=['accuracy'])
+model.summary()
 
 solved_puzzles = open('valid puzzles','r').read().split('\n')[:-1]
 
-best_ar = 0
-herstory = {'predictive_validity':[],'autoregressive_validation':[]}
+batches = 10000
+batch_size = 128
+samples = batch_size * batches
+resamples = samples//5
+best_pred_acc = 0
+puzzles_for_review = []
+herstory = {'predictive_acc':[],'autoregressive_acc':[]}
 for e in range(1,1000):
-	puzzles, solutions, pencilmarks = create_dataset(solved_puzzles, 100000)
-	history = model.fit(pencilmarks, solutions, epochs=1, verbose=1, validation_split=0.10)
+	puzzles, solutions, pencilmarks = create_dataset(solved_puzzles, samples, puzzles_for_review=puzzles_for_review)
+	history = model.fit(puzzles, solutions, batch_size=batch_size, epochs=1, verbose=1)
 
-	puzzles, solutions, pencilmarks = create_dataset(solved_puzzles, 1000, True)
-	herstory['predictive_validity'] += [validate_predictions(puzzles, solutions, pencilmarks, model)]
+	predictions = model.predict(puzzles)
+	puzzles_for_review = get_hardest_n_puzzles_by_mae(puzzles, solutions, predictions, resamples)
 
-	puzzles, solutions, pencilmarks = create_dataset(solved_puzzles, 100, True)
-	herstory['autoregressive_validation'] += [autoregressive_validation(puzzles, solutions, pencilmarks, model)]
+	puzzles, solutions, pencilmarks = create_dataset(solved_puzzles, 5000, True, [])
+	herstory['predictive_acc'] += [validate_predictions(puzzles, solutions, pencilmarks, model)]
+
+	puzzles, solutions, pencilmarks = create_dataset(solved_puzzles, 500, True, [])
+	herstory['autoregressive_acc'] += [autoregressive_validation(puzzles, solutions, pencilmarks, model)]
 
 	for key,value in history.history.items():
 		if key in herstory:
 			herstory[key] += value
 		else:
 			herstory[key] = value
-	
+
 	for key,value in herstory.items():
-		if key.find('val') != -1:
-			print (key,' '.join([str(x)[:6] for x in value]))
+		print (key,' '.join([str(x)[:6] for x in value]))
 
-	if best_ar < herstory['autoregressive_validation'][-1]:
-		best_ar = herstory['autoregressive_validation'][-1]
-		model.save('model - ar='+str(best_ar)[:6])
+	if best_pred_acc < herstory['predictive_acc'][-1]:
+		best_pred_acc = herstory['predictive_acc'][-1]
+		model.save('model - one-shot_acc='+str(best_pred_acc)[:6])
 		print ('model saved')
-
 
 
 
