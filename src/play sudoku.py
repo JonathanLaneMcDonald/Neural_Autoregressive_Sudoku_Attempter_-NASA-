8e38f145387184f1c48edeadb9332b83f200c720
Lane McDonald <Jonathan.Lane.McDonald@gmail.com>
2020-10-17 15:40:03 +1100

    update formatting and naming

--- src/play sudoku.py
+++ src/play sudoku.py
@@ -10,17 +10,20 @@ from tkinter import *
 
 SQ = 80
 
-h = '0123456789abcdef'
+
 def ftoh(f):
+	h = '0123456789abcdef'
 	num = int(f*255)
-	a = num//16
-	b = num%16
+	a = num // 16
+	b = num % 16
 	return h[a] + h[b]
 
-class display( Frame ):
+
+class SudokuUI(Frame):
 
 	def draw(self):
-		''' Draw the pencil marks and use a model for annotation if the user wishes '''
+		""" Draw the pencil marks and use a model for annotation if the user wishes """
+
 		self.canvas.delete('all')
 		
 		if self.update_annotation:
@@ -31,93 +34,119 @@ class display( Frame ):
 			self.update_annotation = False
 		
 		# draw cursor position
-		self.canvas.create_rectangle(self.col*SQ,self.row*SQ,(self.col+1)*SQ,(self.row+1)*SQ,fill='#ffff88')
+		self.canvas.create_rectangle(self.col*SQ, self.row*SQ, (self.col+1)*SQ, (self.row+1)*SQ, fill='#ffff88')
 		
 		# draw lines
 		for line in range(9):
 			if line % 3 == 0:
-				self.canvas.create_line(line*SQ,0,line*SQ,9*SQ,fill='black',width=3)
-				self.canvas.create_line(0,line*SQ,9*SQ,line*SQ,fill='black',width=3)
+				self.canvas.create_line(line*SQ, 0, line*SQ, 9*SQ, fill='black', width=3)
+				self.canvas.create_line(0, line*SQ, 9*SQ, line*SQ, fill='black', width=3)
 			else:
-				self.canvas.create_line(line*SQ,0,line*SQ,9*SQ,fill='black', dash=(5,5))
-				self.canvas.create_line(0,line*SQ,9*SQ,line*SQ,fill='black', dash=(5,5))
+				self.canvas.create_line(line*SQ, 0, line*SQ, 9*SQ, fill='black', dash=(5, 5))
+				self.canvas.create_line(0, line*SQ, 9*SQ, line*SQ, fill='black', dash=(5, 5))
 
-		txt = {	0:(2*SQ//10,2*SQ//10),
-				1:(5*SQ//10,2*SQ//10),
-				2:(8*SQ//10,2*SQ//10),
-				3:(2*SQ//10,5*SQ//10),
-				4:(5*SQ//10,5*SQ//10),
-				5:(8*SQ//10,5*SQ//10),
-				6:(2*SQ//10,8*SQ//10),
-				7:(5*SQ//10,8*SQ//10),
-				8:(8*SQ//10,8*SQ//10)}
+		txt = {
+			0: (2*SQ // 10, 2*SQ // 10),
+			1: (5*SQ // 10, 2*SQ // 10),
+			2: (8*SQ // 10, 2*SQ // 10),
+			3: (2*SQ // 10, 5*SQ // 10),
+			4: (5*SQ // 10, 5*SQ // 10),
+			5: (8*SQ // 10, 5*SQ // 10),
+			6: (2*SQ // 10, 8*SQ // 10),
+			7: (5*SQ // 10, 8*SQ // 10),
+			8: (8*SQ // 10, 8*SQ // 10)	}
 		
 		for r in range(9):
 			for c in range(9):
 				if self.show_solution:
-					self.canvas.create_text(c*SQ+SQ//2,r*SQ+SQ//2,font=self.assign_font,text=str(np.argmax(self.solution[r][c])+1))
+					self.canvas.create_text(
+						c*SQ+SQ // 2, r*SQ+SQ // 2, font=self.assign_font,
+						text=str(np.argmax(self.solution[r][c])+1))
 				else:
 					if max(self.board[r][c]) == 1:
-						self.canvas.create_text(c*SQ+SQ//2,r*SQ+SQ//2,font=self.assign_font,text=str(np.argmax(self.board[r][c])+1))
+						self.canvas.create_text(
+							c*SQ+SQ // 2, r*SQ+SQ // 2, font=self.assign_font,
+							text=str(np.argmax(self.board[r][c])+1))
 					else:
 						prefs = self.annotation[r][c]
 						for p in range(9):
-							value = 1-prefs[p]#**2
-							self.canvas.create_text(c*SQ + txt[p][0], r*SQ + txt[p][1],font=self.pencil_font,text=str(p+1), fill='#ff'+ftoh(value)+ftoh(value))
+							value = 1-prefs[p]
+							if prefs[p] > 0.98:
+								self.canvas.create_text(
+									c*SQ + txt[p][0], r*SQ + txt[p][1], font=self.pencil_font,
+									text=str(p+1), fill='#'+ftoh(value)+ftoh(value)+'ff')
+							else:
+								self.canvas.create_text(
+									c*SQ + txt[p][0], r*SQ + txt[p][1], font=self.pencil_font,
+									text=str(p+1), fill='#ff'+ftoh(value)+ftoh(value))
 						
 		self.canvas.update_idletasks()
 	
 	def generate_pencil_marks(self):
-		''' Given a puzzle, generate pencil marks by doing basic elimination. '''
-		# generate pencil marks for rows, cols, and blocks, then "and" them together for each position
-		row_marks = [set(range(9)) - set([np.argmax(self.board[x//9][x%9]) for x in r if np.max(self.board[x//9][x%9]) == 1]) for r in rows]
-		col_marks = [set(range(9)) - set([np.argmax(self.board[x//9][x%9]) for x in c if np.max(self.board[x//9][x%9]) == 1]) for c in cols]
-		blk_marks = [set(range(9)) - set([np.argmax(self.board[x//9][x%9]) for x in b if np.max(self.board[x//9][x%9]) == 1]) for b in blks]
+		""" Given a puzzle, generate pencil marks by doing basic elimination. """
 
-		pencil_marks = np.zeros((9,9,9))
+		# generate pencil marks for rows, cols, and blocks, then "and" them together for each position
+		row_marks = [set(range(9)) - set([np.argmax(self.board[x // 9][x % 9]) for x in r if np.max(self.board[x // 9][x % 9]) == 1]) for r in rows]
+		col_marks = [set(range(9)) - set([np.argmax(self.board[x // 9][x % 9]) for x in c if np.max(self.board[x // 9][x % 9]) == 1]) for c in cols]
+		blk_marks = [set(range(9)) - set([np.argmax(self.board[x // 9][x % 9]) for x in b if np.max(self.board[x // 9][x % 9]) == 1]) for b in blks]
+
+		pencil_marks = np.zeros((9, 9, 9))
 		for m in range(81):
-			r, c = m//9, m%9
+			r = m // 9
+			c = m % 9
 			for i in row_marks[membership[m][0]].intersection(col_marks[membership[m][1]]).intersection(blk_marks[membership[m][2]]):
 				pencil_marks[r][c][i] = 1
 		
 		return pencil_marks + self.board
 
 	def hard_reset(self):
-		''' Reset everything to zeros '''
-		self.board = np.zeros((9,9,9))
-		self.solution = np.zeros((9,9,9))
+		""" Reset everything to zeros """
+
+		self.board = np.zeros((9, 9, 9))
+		self.solution = np.zeros((9, 9, 9))
 		self.update_annotation = True
 
 	def reset_puzzle(self):
-		''' Repopulate everything with a random puzzle '''
+		""" Repopulate everything with a random puzzle """
+
 		selection = int(npr()*len(self.puzzle_collection))
 		puzzle = [int(x) for x in self.puzzle_collection[selection].split('\t')[0]]
 		solution = [int(x) for x in self.puzzle_collection[selection].split('\t')[1]]
 		
-		self.board = np.zeros((9,9,9))
+		self.board = np.zeros((9, 9, 9))
 		for p in range(81):
-			r, c = p//9, p%9
+			r = p // 9
+			c = p % 9
 			if puzzle[p]:
 				self.board[r][c][puzzle[p]-1] = 1
 
-		self.solution = np.zeros((9,9,9))
+		self.solution = np.zeros((9, 9, 9))
 		for p in range(81):
-			r, c = p//9, p%9
+			r = p // 9
+			c = p % 9
 			if solution[p]:
 				self.solution[r][c][solution[p]-1] = 1
 
 		self.update_annotation = True
 
 	def keyboard(self, event):
-		''' Handle keyboard input. Doubles as a user manual ;) '''
-		if event.keysym == 'Up':			self.row -= 1
-		if event.keysym == 'Down':			self.row += 1
-		if event.keysym == 'Left':			self.col -= 1
-		if event.keysym == 'Right':			self.col += 1
+		""" Handle keyboard input. Doubles as a user manual ;) """
+
+		if event.keysym == 'Up':
+			self.row -= 1
+		if event.keysym == 'Down':
+			self.row += 1
+		if event.keysym == 'Left':
+			self.col -= 1
+		if event.keysym == 'Right':
+			self.col += 1
 		
-		if event.keysym == 'r':				self.reset_puzzle()
-		if event.keysym == 'R':				self.hard_reset()
-		if event.keysym == 's':				self.show_solution ^= 1
+		if event.keysym == 'r':
+			self.reset_puzzle()
+		if event.keysym == 'R':
+			self.hard_reset()
+		if event.keysym == 's':
+			self.show_solution ^= 1
 
 		if event.keysym == 'h':
 			self.annotate_with_model ^= 1
@@ -138,26 +167,28 @@ class display( Frame ):
 		self.row = max(0, min(8, self.row))
 		self.col = max(0, min(8, self.col))
 
-		print (event.keysym, event.char)
+		print(event.keysym, event.char)
 
 		self.draw()
 	
 	def mouse_update(self, event):
-		''' Have the cursor follow the mouse around '''
-		self.row = int(event.y//SQ)
-		self.col = int(event.x//SQ)
+		""" Have the cursor follow the mouse around """
+
+		self.row = int(event.y // SQ)
+		self.col = int(event.x // SQ)
 		self.draw()
 
 	def invent_canvas(self):
-		''' Set up the canvas '''
+		""" Set up the canvas """
+
 		Frame.__init__(self)
 		self.master.title('Lane\'s Sudoku Helper')
-		self.master.rowconfigure(0,weight=1)
-		self.master.columnconfigure(0,weight=1)
+		self.master.rowconfigure(0, weight=1)
+		self.master.columnconfigure(0, weight=1)
 		self.grid(sticky=N+S+E+W)
 
-		self.canvas=Canvas(self,width=9*SQ, height=9*SQ)
-		self.canvas.grid(row=0,column=0)
+		self.canvas = Canvas(self, width=9*SQ, height=9*SQ)
+		self.canvas.grid(row=0, column=0)
 
 		self.bind_all('<KeyPress>', self.keyboard)
 		self.bind_all('<Motion>', self.mouse_update)
@@ -186,4 +217,5 @@ class display( Frame ):
 		self.reset_puzzle()
 		self.invent_canvas()
 
-display().mainloop()
+
+SudokuUI().mainloop()
